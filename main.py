4f9e1476e08422ad81edfd673cf6f9e5faf248fd
IanEdwards99 <EDWIAN004@myuct.ac.za>
2021-05-19 18:07:39 +0200

    Created scatter, write to file and get distance functions

--- main.py
+++ main.py
@@ -14,7 +14,53 @@ class KMeans:
         self.tol = tol
 
 
+    def getDistance(self, pair1, pair2):
+        return math.sqrt((pair1[0]-pair2[0])**2 + (pair1[1]-pair2[1])**2)
 
+    def writeToFile(self, iteration, clusters, centroids):
+        f = open("K-means.txt", "a")
+        f.write("Iteration " + str(iteration)+"\n\n")
+        for i in clusters:
+            output = "Cluster " + str(i) + ": "
+            for j in range(0, len(clusters[i])):
+                feature = clusters[i][j]
+                key_list = list(data.keys())
+                val_list = list(data.values())
+                pos = val_list.index(feature)
+                key = key_list[pos]
+                if j == len(clusters[i])-1:
+                    output = output + str(key)
+                else: output = output + str(key) + ", "
+            
+            f.write(output+"\n")
+            f.write("Centroid: (" + str(round(centroids[i][0], 2)) + ", " + str(round(centroids[i][1],2)) + ")"+"\n\n")
+            output = ""
+
+        f.close()
+
+    def drawScatter(self):
+        iterations, clusters, centroids = self.fit()
+        mydata = []
+        for i in clusters:
+            mydata.append(np.array(clusters[i]))
+        x,y = mydata[0].T
+        plot1 = plt.figure(1)
+        plt.scatter(x,y, c='b')
+        x,y = mydata[1].T
+        plt.scatter(x,y, c='r')
+        x,y = mydata[2].T
+        plt.scatter(x,y, c='y')
+
+
+        totaldata = []
+        for i in data:
+            totaldata.append(data[i])
+
+        totaldata = np.array(totaldata)
+        plot2 = plt.figure(2)
+        x, y = totaldata.T
+        plt.scatter(x,y, c = 'g')
+        plt.show()
 
     def fit(self):
         f = open("K-means.txt", "w")
